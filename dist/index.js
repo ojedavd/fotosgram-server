695535342c9479641c8ec637dcd62eb77d799bcc
ssvictoro <ojedavd@hotmail.com>
2019-09-05 14:20:12 -0300

    Crear servicio REST en Express

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
+const usuario_1 = __importDefault(require("./routes/usuario"));
 const server = new server_1.default();
+server.app.use('/user', usuario_1.default);
 server.start(() => {
     console.log(`Servidor corriendo en el puerto ${server.port}`);
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import Server from "./classes/server";
+import userRoutes from "./routes/usuario";
 
 const server = new Server();
+
+server.app.use('/user', userRoutes);
 
 server.start( () => {
     console.log(`Servidor corriendo en el puerto ${ server.port }`);
