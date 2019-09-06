70b6146cb1cf74e5a6a4c99b54b1460d0ba71e6a
ssvictoro <ojedavd@hotmail.com>
2019-09-06 13:24:19 -0300

    Actualizar usuario en la BD

--- routes/usuario.ts
+++ routes/usuario.ts
@@ -81,9 +81,34 @@ userRoutes.post('/create', (req: Request, res: Response) => {
 // actualizar usuario
 userRoutes.post('/update', verificaToken, (req: any, res: Response) => {
     
-    res.json({
-        ok: true,
-        usuario: req.usuario
+    const user = {
+        nombre: req.body.nombre || req.usuario.nombre,
+        email: req.body.email || req.usuario.email,
+        avatar: req.body.avatar || req.usuario.avatar
+    };
+
+    Usuario.findByIdAndUpdate( req.usuario._id, user, { new: true }, (err, userDB) =>{
+        if ( err ) throw err;
+
+        if ( !userDB ) {
+            return res.json({
+                ok: false,
+                mensaje: 'No existe un usuario con ese ID'
+            })
+        }
+
+        const tokenUser = Token.getJwtToken({
+            _id: userDB._id,
+            nombre: userDB.nombre,
+            email: userDB.email,
+            avatar: userDB.avatar
+        });
+
+        res.json({
+            ok: true,
+            token: tokenUser
+        });
+
     });
 
 });
